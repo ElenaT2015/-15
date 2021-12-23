95f9db8b8b14538488d0a9d4a7660956d12670bc
Tsygankovae.a <Tsygankovae.a@outlook.com>
2021-12-23 23:33:01 +0300

    Добавьте файлы проекта.

---
+++ Задание 15/ISeries.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_15
+{
+    /*
+      метод void setStart(int x) - устанавливает начальное значение
+   метод int getNext() - возвращает следующее число ряда
+    метод void reset() - сброс к начальному значению
+     */
+    interface Iseries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+
+    class ArithProgression : Iseries
+    {
+
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue += step;
+            return currentValue; throw new NotImplementedException();
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ Задание 15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_15
+{
+    class Progrram
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();// Создаем арифметическую прогрессию//
+            arith.setStart(10); // Устанавливаем стартовое значение//
+            arith.setStep(2); // Устанавливаем  шаг//
+            Console.WriteLine(arith.getNext());// вывод на кран значение арифметической прогрессии//
+            Console.WriteLine(arith.getNext());// вывод на кран последующее значение арифметической прогрессии//
+            Console.WriteLine(arith.getNext());// вывод на кран последующее значение арифметической прогрессии//
+            arith. reset();// Срасываем к стартовому значению//
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.ReadKey();
+        }
+    }
+}
+
+
